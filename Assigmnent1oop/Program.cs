3796da6e32b3f1cffa22de2693f270fc9778da83
Noran Salah <noransalah840@gmail.com>
2024-12-30 21:44:58 +0200

    Add project files.

---
+++ Assigmnent1oop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assigmnent1oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
